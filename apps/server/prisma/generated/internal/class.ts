6962b04946c7b4561d703fc4ea6e924bdc4cdcc0
kalyXY <peter23xp@gmail.com>
2025-08-09 18:09:32 +0300

    Update Prisma client configuration for Windows environment
    - Changed binary target from 'debian-openssl-3.0.x' to 'windows'.
    - Updated output path for generated files to reflect local Windows directory structure.
    - Added new Windows query engine binary file.

--- apps/server/prisma/generated/internal/class.ts
+++ apps/server/prisma/generated/internal/class.ts
@@ -22,7 +22,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "/workspace/apps/server/prisma/generated",
+      "value": "C:\\Users\\Peter AKILIMALI\\OneDrive\\Documents\\ISJ\\ISJ\\apps\\server\\prisma\\generated",
       "fromEnvVar": null
     },
     "config": {
@@ -32,12 +32,12 @@ const config: runtime.GetPrismaClientConfig = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "debian-openssl-3.0.x",
+        "value": "windows",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/workspace/apps/server/prisma/schema/schema.prisma",
+    "sourceFilePath": "C:\\Users\\Peter AKILIMALI\\OneDrive\\Documents\\ISJ\\ISJ\\apps\\server\\prisma\\schema\\schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../schema",
@@ -47,7 +47,6 @@ const config: runtime.GetPrismaClientConfig = {
     "db"
   ],
   "activeProvider": "mongodb",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
